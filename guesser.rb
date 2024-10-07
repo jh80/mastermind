7d2ba94d2b1c2d1f3b36e7c4255f3f8c5829c67c
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-07 15:19:33 -0700

    Create Guesser class

---
+++ guesser.rb
@@ -0,0 +1,15 @@
+class Guesser
+  def initialize 
+    @guess_string = ""
+    @guess_array = []
+  end 
+
+  def get_guess
+    puts "What is your guess?"
+    gets.chomp
+  end
+
+  def format_guess(guess_string)
+    @guess_array =  guess_string.split
+  end
+end
