d8344ab042bba32589fe36c402b0919c687b49c7
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-08 11:56:34 -0700

    Count right color wrong place

--- checks.rb
+++ checks.rb
@@ -11,9 +11,19 @@ module Checks
     rc_rp_indexes
   end
 
-  def remove_elements_by_index(array, indexes)
+  def remove_elements_by_indexes(array, indexes)
     array.select.with_index do |_, index|
        !indexes.include?(index)
     end
+  end
+
+  def count_rc_wp(filtered_code, filtered_guess)
+    all_colors = ["R", "Y", "G", "U", "W", "B"]
+    rc_wp_count = 0
+    all_colors.each do |color|
+      next if filtered_code.count(color).zero? 
+      rc_wp_count += filtered_code.count(color) >= filtered_guess.count(color) ? filtered_guess.count(color) : filtered_code.count(color)
+    end
+    rc_wp_count
   end
 end
