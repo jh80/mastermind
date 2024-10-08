f45bc884f039208305e8d98630275756143fa223
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-08 16:22:03 -0700

    Add a message when guesses run out

--- game.rb
+++ game.rb
@@ -3,14 +3,19 @@ class Game
     @guesser = Guesser.new
     @code_maker = CodeMaker.new
     @rc_rp = 0
+    @guess_max = 12
   end
 
   def play
     # Code is created during initialization phase when code_maker instance is created
-    while @rc_rp != @code_maker.code.length
+    @guess_max.times do 
+      if @rc_rp == @code_maker.code.length
+        puts "Congratultions the code was #{@code_maker.code} and you guessed it!"
+        break
+      end
       @guesser.make_guess
       @rc_rp = @code_maker.evaluate_guess(@guesser.guess)
     end
-    puts "Congratultions the code was #{@code_maker.code} and you guessed it!"
+    puts "Sorry, you ran out of guesses"
   end
 end
