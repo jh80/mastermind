8d01f6bbb7c61f463cc7cffb7003c88e1782cd3f
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-08 16:01:10 -0700

    Cycle through guesses until the correct guess

--- game.rb
+++ game.rb
@@ -1,5 +1,16 @@
 class Game
   def initialize
     @guesser = Guesser.new
+    @code_maker = CodeMaker.new
+    @rc_rp = 0
+  end
+
+  def play
+    # Code is created during initialization phase when code_maker instance is created
+    while @rc_rp != @code_maker.code.length
+      @guesser.make_guess
+      @rc_rp = @code_maker.evaluate_guess(@guesser.guess)
+    end
+    puts "Congratultions the code was #{@code_maker.code} and you guessed it!"
   end
 end
