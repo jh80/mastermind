57ee1ec30f7e74a29c3750d2bda93054c234f083
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-08 17:43:20 -0700

    Create code automating method

--- code_maker.rb
+++ code_maker.rb
@@ -7,5 +7,15 @@ class CodeMaker
   attr_reader :code
   def initialize
     @code = ["R", "U", "G", "R"]
+    @code_length = 4
+  end
+
+  def generate_code
+    code = []
+    all_colors = ["R", "Y", "G", "U", "W", "B"]
+    @code_length.times do
+      code.push(all_colors.sample)
+    end
+    code
   end
 end
