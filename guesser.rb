b7aad738e38e2bc571dc825665a9e35726453026
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-10 10:01:34 -0700

    Ensure guess is in correct format

--- guesser.rb
+++ guesser.rb
@@ -10,8 +10,13 @@ class Guesser
   end 
 
   def get_guess
-    puts "What is your guess?"
-    gets.chomp
+    guess = ""
+    loop do
+      puts "What is your guess?"
+      guess = gets.chomp
+      break unless guess_issue?(guess)
+    end
+    guess
   end
 
   def format_guess(guess_string)
@@ -21,5 +26,26 @@ class Guesser
   def make_guess
     @guess_string = get_guess
     format_guess(@guess_string)
+  end
+
+  def all_chars_valid?(guess)
+    all_colors = ["R", "Y", "G", "U", "W", "B"]
+    all_chars_valid = true
+    guess.upcase.each_char { |char| all_chars_valid = false unless all_colors.include?(char)}
+    all_chars_valid
+  end
+
+  def guess_issue?(guess)
+    if !all_chars_valid?(guess)
+      puts "The available letters to represent the colors are: R Y G U B W"
+      return true
+    elsif guess.length > 4
+      puts "Your guess is too long, it should only be 4 characters. Remember not to use spaces."
+      return true
+    elsif guess.length < 4
+      puts "Your guess is not longe enough, use 4 letters"
+      return true
+    end
+    return false
   end
 end
