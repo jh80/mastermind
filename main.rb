30233f99813e3d5a468972bccf6b7795800407fd
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-10 10:51:43 -0700

    Set up main.rb to create and play game

--- main.rb
+++ main.rb
@@ -1,9 +1,23 @@
+require 'pry-byebug'
 require './guesser.rb'
+require './code_maker.rb'
+require './checks.rb'
+require './game.rb'
 
-code = ["R", "U", "R", "W"]
 
+game = Game.new
+game.play
 
-guesser = Guesser.new()
-guess_string = guesser.get_guess
-guess_array = guesser.format_guess(guess_string)
-p "Your guess is #{guess_array}"
+# guesser = Guesser.new()
+# code_maker = CodeMaker.new()
+# guesser.make_guess
+# p guesser.guess
+# code_maker.evaluate_guess(guesser.guess)
+
+# p indexes = guesser.find_rc_rp_indexes(code, guess)
+# filtered_code = guesser.remove_elements_by_indexes(code, indexes)
+# p "Leftover code array #{filtered_code}"
+# filtered_guess = guesser.remove_elements_by_indexes(guess, indexes)
+# p "leftover guess array #{filtered_guess}"
+# guesser.print_hint(guesser.calculate_rp_rc_count(guess, filtered_guess), guesser.count_rc_wp(filtered_code, filtered_guess))
+
