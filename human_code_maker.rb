e5765bcbe31477ded9a7867984feab2177b91dc3
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-15 19:44:41 -0700

    Give HumanCodeMaker module access to UserCompliance and use access

---
+++ human_code_maker.rb
@@ -0,0 +1,14 @@
+require './user_compliance.rb'
+
+module HumanCodeMaker
+  include UserCompliance
+
+  def get_human_code
+    code = ""
+    loop do
+      puts "Please enter your code below:"
+      code = gets.chomp
+      break unless code_format_issue?(code)
+    end
+  end
+end
