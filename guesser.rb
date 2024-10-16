6fb9ffecda4aa658924a79bbef5cf54ee839f95a
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-15 19:29:22 -0700

    Create UserCompliance module

--- guesser.rb
+++ guesser.rb
@@ -34,18 +34,4 @@ class Guesser
     guess.upcase.each_char { |char| all_chars_valid = false unless all_colors.include?(char)}
     all_chars_valid
   end
-
-  def code_format_issue?(guess)
-    if !all_chars_valid?(guess)
-      puts "The available letters to represent the colors are: R Y G U B W"
-      return true
-    elsif guess.length > 4
-      puts "Your guess is too long, it should only be 4 characters. Remember not to use spaces."
-      return true
-    elsif guess.length < 4
-      puts "Your guess is not longe enough, use 4 letters"
-      return true
-    end
-    return false
-  end
 end
