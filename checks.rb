f581351153e78b3325aa8590ce8fac81f04f00e5
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-19 10:04:30 -0700

    Take printing out of evaluate_guess

--- checks.rb
+++ checks.rb
@@ -43,7 +43,6 @@ module Checks
     filtered_guess = remove_elements_by_indexes(guess, rc_rp_indexes)
     rc_rp_count = calculate_rp_rc_count(guess, filtered_guess)
     rc_wp_count = count_rc_wp(filtered_code, filtered_guess)
-    print_hint(rc_rp_count, rc_wp_count)
     {rc_rp: rc_rp_count, rc_wp: rc_wp_count}
   end
 end
