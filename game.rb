ac71444f062637f61e9b5385fd125d7d6decdc54
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-20 11:42:28 -0700

    Print current viable codes list after hint prints

--- game.rb
+++ game.rb
@@ -39,14 +39,15 @@ class Game
         puts "Congratultions the code was #{@code_maker.code.join} and you guessed it in #{round} guesses!"
         break
       end
-      @guesser.update_viable_codes(real_world) if round > 0
       guess = @guesser.set_guess(@real_world, round)
       print "guess: #{guess.join}  "
       results = @code_maker.evaluate_guess(@guesser.guess, @code_maker.code)
       @rc_rp = results[:rc_rp]
       @code_maker.print_hint(results[:rc_rp], results[:rc_wp])
       @real_world.add_rule(guess, results[:rc_rp], results[:rc_wp])
-      @real_world.process_results(@real_world.rules[-1])
+      @guesser.update_viable_codes(real_world)
+      print "Options left: #{@real_world.viable_codes}\n\n"
+      #@real_world.process_results(@real_world.rules[-1])ruby ma
       puts "Sorry, you ran out of guesses" if round == @guess_max - 1
     end
   end
