caf6abd2daf03e2f8970d7a3de63b5b04449f7b8
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-21 13:54:12 -0700

    Add comments about reorganizing human vs computer features

--- main.rb
+++ main.rb
@@ -8,8 +8,8 @@ require './world.rb'
 
 game = Game.new
 real_world = World.new
-p real_world.available_colors_collection
-#game.play
+
+game.play
 
 # code_maker = game.code_maker.choose_code_maker
 # guesser = Guesser.new()
@@ -25,3 +25,35 @@ p real_world.available_colors_collection
 # p "leftover guess array #{filtered_guess}"
 # guesser.print_hint(guesser.calculate_rp_rc_count(guess, filtered_guess), guesser.count_rc_wp(filtered_code, filtered_guess))
 
+# TO DO a possible start to reorganizing how the computer vs human features are called
+# def play_computer_guesser
+#   @guess_max.times do |round|
+#     if @rc_rp == @code_maker.code.length
+#       puts "Congratultions the code was #{@code_maker.code.join} and you guessed it in #{round} guesses!"
+#       break
+#     end
+#     guess = @guesser.set_guess(@real_world, round)
+#     print "guess: #{guess.join}  "
+#     results = @code_maker.evaluate_guess(@guesser.guess, @code_maker.code)
+#     @rc_rp = results[:rc_rp]
+#     @code_maker.print_hint(results[:rc_rp], results[:rc_wp])
+#     print "Options left: #{@real_world.viable_codes}\n\n"
+#     puts "Sorry, you ran out of guesses" if round == @guess_max - 1
+#   end
+# end
+
+# def play_human_guesser
+#   @guess_max.times do |round|
+#     if @rc_rp == @code_maker.code.length
+#       puts "Congratultions the code was #{@code_maker.code.join} and you guessed it in #{round} guesses!"
+#       break
+#     end
+#     guess = @guesser.set_guess(@real_world, round)
+#     print "guess: #{guess.join}  "
+#     results = @code_maker.evaluate_guess(@guesser.guess, @code_maker.code)
+#     @rc_rp = results[:rc_rp]
+#     @code_maker.print_hint(results[:rc_rp], results[:rc_wp])
+#     print "Options left: #{@real_world.viable_codes}\n\n"
+#     puts "Sorry, you ran out of guesses" if round == @guess_max - 1
+#   end
+# end
